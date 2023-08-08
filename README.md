38a8a93b851f77b963dd7782f229a8d8dad7bf09
Beyond <beyondbeyond602@gmail.com>
2023-08-08 17:55:19 +0100

    Initial commit for component interaction

---
+++ README.md
@@ -0,0 +1,20 @@
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Step outline
+
+1. Explain files structure and purpose
+2. Explain Component and its interactions
+3. Form and Hooks
+4. Mapping, listing and manipulation
+5. State and Hooks
+6. Store and context
+7. Routing (Pages)
+8. API
+9. External libraries
---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+const InitialContent =()=>{
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank"  rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank"  rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      </>
+  )
+}
+
+
+function App() {
+  return (
+    <>
+      <InitialContent />
+      <OurApp />
+      <OurApp />
+      <OurApp />
+      <OurApp />
+      <OurApp />
+    </>
+  )
+}
+
+export default App
+
+
+export function OurApp(){
+  const [test, setTest] = useState(0)
+  return (
+    <>
+      <div>This is  our app in a component</div>
+      <button onClick={()=>setTest(test+1)}>{test}</button>
+    </>
+  )
+}
+
+
